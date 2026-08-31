88a2e2b3b344c41670e90e12e6516c9ade2d2334
Zmirkovic <zeno.mirkovic@gmail.com>
2026-08-31 11:38:24 +0200

    Update git_exercise.py

--- challenge_day1/resources/git_exercise.py
+++ challenge_day1/resources/git_exercise.py
@@ -1,20 +1,4 @@
 import sys
-
-
-sys.path.append("challenge_day3")
-
-
-from challenge_day3 import lara 
-
-import sys
-
-
-sys.path.append("challenge_day3")
-
-
-from challenge_day3 import lol
-
-import sys
 sys.path.append("challenge_day3")
 
 
