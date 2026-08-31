e4ac3a2f00b89c866ca3476e3105cf1b4269ad98
Zmirkovic <zeno.mirkovic@gmail.com>
2026-08-31 12:05:44 +0200

    Update name.py

--- challenge_day3/name.py
+++ challenge_day3/name.py
@@ -2,8 +2,3 @@ def maname():
     name = "Zeno"
     print(name)
     return print(name)
-
-
-def exercize():
-    teamname = "Brobots"
-    return print(f"This is team {teamname} and we are: {maname()}")
