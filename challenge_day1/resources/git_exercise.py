e8f498cb7cbd06fe447eee8f30dfeaebfb1e77bf
larapoelmann-png <larapoelmann@gmail.com>
2026-08-31 11:24:45 +0200

    Update git_exercise.py

---
+++ challenge_day1/resources/git_exercise.py
@@ -0,0 +1,15 @@
+import sys
+
+
+sys.path.append("challenge_day3")
+
+
+from challenge_day3 import lara.py  
+
+import sys
+
+
+sys.path.append("challenge_day3")
+
+
+from challenge_day3 import lol.py  
