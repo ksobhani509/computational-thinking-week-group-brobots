a95d3664d26f64022c06f07d08981136baa7d5d1
larapoelmann-png <larapoelmann@gmail.com>
2026-08-31 11:52:14 +0200

    Create git_exercise.py

---
+++ challenge_day3/git_exercise.py
@@ -0,0 +1,21 @@
+
+import sys
+# sys.path.append("challenge_day1")
+
+
+from lara import laraname
+
+
+laraname()
+
+from lol import sname
+
+sname()
+
+from luc_name import name
+
+name()
+
+from name import maname
+
+maname()
