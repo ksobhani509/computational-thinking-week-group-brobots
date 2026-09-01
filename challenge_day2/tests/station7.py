eba22186a010d9bba90908b0257afda05af4a7b7
larapoelmann-png <larapoelmann@gmail.com>
2026-09-01 14:43:15 +0200

    Update station7.py

--- challenge_day2/tests/station7.py
+++ challenge_day2/tests/station7.py
@@ -1,4 +1,16 @@
+def solution_station_7(a,b,c,d,e):
+    results=b+d
+    return results
+    #define the inputs
+
 a=3
 b=-1
 c=4
 d=7
+e=0.5
+
+output=solution_station_7(a,b,c,d,e)
+print(output) 
+
+
+
