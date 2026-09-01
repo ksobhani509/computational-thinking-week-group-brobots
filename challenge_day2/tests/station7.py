144e9566223fda04c7f42805bb199f9f195bf77b
larapoelmann-png <larapoelmann@gmail.com>
2026-09-01 15:35:02 +0200

    Update station7.py

--- challenge_day2/tests/station7.py
+++ challenge_day2/tests/station7.py
@@ -1,15 +1,16 @@
-def solution_station_7(input):
+def solution_station_7(expression):
+    values = {
+        "a": 3,
+        "b": 4,
+        "c": -1,
+        "d": 7,
+        "e": 0.5,
+    }
+    
+    return eval(expression, {"__builtins__": None}, values)
 
-
-a=3
-b=-1
-c=4
-d=7
-e=0.5
-
-output= eval(input)
-print(output) 
-
-
+#example   
+print(solution_station_7("b + d")) 
+print(solution_station_7("a * e + c"))  
 
 
