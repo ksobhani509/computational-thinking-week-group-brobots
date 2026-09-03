94f895480160d1272cc9c0b1c46f0b53809d9301
kubi248 <jakub.homa@student.uva.nl>
2026-09-03 14:04:38 +0200

    Step5 solved

--- challenge_day4/Lua_step4/summary.lua
+++ challenge_day4/Lua_step4/summary.lua
@@ -11,7 +11,7 @@ SummaryMetaTable = {
 
 -- Read data from `data4.txt`
 local lines = {}
-for line in io.lines("data4.txt") do
+for line in io.lines("../testdata/data4.txt") do
     table.insert(lines, line)
 end
 
